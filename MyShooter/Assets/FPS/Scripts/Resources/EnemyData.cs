4f2e4b30432cc81b3567550a749e7992368a8614
gldimion <gld-lux@yandex.ru>
2019-02-18 00:15:13 +0300

    save and load resources

---
+++ MyShooter/Assets/FPS/Scripts/Resources/EnemyData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData
+{
+    public string PrefabName;
+    public Vector3 Pos;
+    public Vector3 Scale;
+    public Quaternion Rot;
+}
---
+++ MyShooter/Assets/FPS/Scripts/Resources/LoadResources.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadResources : MonoBehaviour
+{
+    private void Awake()
+    {
+        Load();
+    }
+
+    private void Load()
+    {
+        string path = Path.Combine(Application.dataPath, "ResourcesData.json");
+        if (!File.Exists(path)) return;
+
+        List<EnemyData> enemies = new List<EnemyData>();
+
+        using (var sr = new StreamReader(path))
+        {
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                enemies.Add(JsonUtility.FromJson<EnemyData>(s));
+            }
+        }
+
+        Debug.Log("Data loaded");
+
+        foreach (var e in enemies)
+        {
+            var prefab = Resources.Load<GameObject>(e.PrefabName);
+            var tempObj = Object.Instantiate(prefab, e.Pos, e.Rot);
+            tempObj.transform.localScale = e.Scale;
+            tempObj.name = e.PrefabName;
+            tempObj.transform.parent = transform;
+        }
+    }
+}

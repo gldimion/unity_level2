9f1af1af607266381e105d48a9fe328b59d992db
gldimion <gld-lux@yandex.ru>
2019-02-28 12:48:02 +0300

    lesson 8 base

---
+++ MyShooter/Assets/Multiplayer/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public Rigidbody BulletPrefab;
+    public Transform FirePoint;
+
+    public float MoveSpeed;
+    public float RotSpeed;
+
+    private void Start()
+    {
+        if(isLocalPlayer)
+        {
+            foreach (var r in GetComponentsInChildren<Renderer>())
+                r.material.color = Color.green;
+        }
+        else
+        {
+            foreach (var r in GetComponentsInChildren<Renderer>())
+                r.material.color = Color.red;
+        }
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * RotSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+
+        transform.Rotate(0, y, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetButtonDown("Fire1")) CmdFire();
+    }
+
+    [Command]
+    private void CmdFire()
+    {
+        var bullet = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+        bullet.velocity = bullet.transform.forward * 10;
+        NetworkServer.Spawn(bullet.gameObject);
+    }
+}
---
+++ MyShooter/Assets/Multiplayer/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour
+{
+    public Transform HealthCanvas;
+    public Image FillImage;
+
+    public int MaxHealth = 100;
+    [SyncVar(hook = "OnHealthChanged")]
+    public int CurrentHealth = 100;
+
+    public void LateUpdate()
+    {
+        HealthCanvas.LookAt(Camera.main.transform);
+    }
+
+    public void GetDamage(int damage)
+    {
+        if (!isServer) return;
+
+        if (CurrentHealth <= 0) return;
+
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0) RpcRespawn();
+    }
+
+    private void OnHealthChanged(int health)
+    {
+        CurrentHealth = health;
+        FillImage.fillAmount = (float)CurrentHealth / MaxHealth;
+    }
+
+    [ClientRpc]
+    public void RpcRespawn()
+    {
+        CurrentHealth = MaxHealth;
+        transform.position = Vector3.up * 2f;
+    }
+}
